5a86616117d0f317e187dbab0d7d8b15a7dc4344
cc-lau <christianlau03@gmail.com>
2023-01-26 22:35:05 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
-import DownloadsList from './Components/DownloadsList/DownloadsList';
-import ExampleDescription from './Components/ExampleDescription/ExampleDescription';
-import Footer from './Components/Footer/Footer';
-import Header from './Components/Header/Header';
-import Search from './Components/Search/Search';
+import "./App.css";
+import DownloadsList from "./Components/DownloadsList/DownloadsList";
+import ExampleDescription from "./Components/ExampleDescription/ExampleDescription";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Search from "./Components/Search/Search";
+import { VideoProvider } from "./VideoContext";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Search />
-      <DownloadsList />
+      <VideoProvider>
+        <Search />
+        <DownloadsList />
+      </VideoProvider>
       <ExampleDescription />
       <Footer />
     </div>
--- src/Components/ExampleDescription/ExampleDescription.js
+++ src/Components/ExampleDescription/ExampleDescription.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ExampleDescription() {
   return (
     <div>
-        
+        {/* Paste. Search. Download. */}
     </div>
   )
 }
