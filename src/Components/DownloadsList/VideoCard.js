ddd7ea1e036b20d93d847b247e155e9f08c7c964
cc-lau <christianlau03@gmail.com>
2023-01-27 11:28:57 -0700

    Video card now displays a link to download the mp3 file

--- src/Components/DownloadsList/VideoCard.js
+++ src/Components/DownloadsList/VideoCard.js
@@ -4,7 +4,16 @@ function VideoCard(props) {
   return (
     <div>
       <div>
-        {props.title} {props.duration}
+        {props.title} {props.duration}{" "}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={props.downloadLink}
+          download
+          className="download_btn"
+        >
+          Download MP3
+        </a>
       </div>
     </div>
   );
