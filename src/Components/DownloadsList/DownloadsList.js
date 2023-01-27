392396bb711bb58d3deff63fe937e79cedce6514
cc-lau <christianlau03@gmail.com>
2023-01-26 22:31:21 -0700

    Downloads List now converts video data into props and is passed into the Video card component to be displayed when video is converted

--- src/Components/DownloadsList/DownloadsList.js
+++ src/Components/DownloadsList/DownloadsList.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from "react";
+import VideoContext from "../../VideoContext";
+import VideoCard from "./VideoCard";
 
 function DownloadsList() {
+  const { videoDownload } = useContext(VideoContext);
+  const [isConverted, setIsConverted] = useState(false);
+  const [videoTitle, setVideoTitle] = useState("");
+  const [videoDuration, setVideoDuration] = useState("");
+
+  function fmtMSS(s) {
+    return (s - (s %= 60)) / 60 + (9 < s ? ":" : ":0") + s;
+  }
+
+  function checkConverted() {
+    if (Object.keys(videoDownload).length !== 0) {
+      setIsConverted(true);
+    }
+  }
+
+  useEffect(() => {
+    const roundedDuration = Math.round(videoDownload.duration);
+    const duration = fmtMSS(roundedDuration);
+    setVideoTitle(videoDownload.title);
+    setVideoDuration(duration);
+    checkConverted();
+  }, [videoDownload]);
+
   return (
     <div>
-        
+      {isConverted ? (
+        <VideoCard title={videoTitle} duration={videoDuration}></VideoCard>
+      ) : null}
     </div>
-  )
+  );
 }
 
-export default DownloadsList
+export default DownloadsList;
