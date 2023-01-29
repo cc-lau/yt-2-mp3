43019d6e1760d9d2911b029fce068efa1b53b24e
cc-lau <christianlau03@gmail.com>
2023-01-29 14:58:01 -0700

    Imported header css module

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import "./HeaderStyles.css"
+
 
 function Header() {
   return (
-    <div>
-        Header
+    <div className='header-container'>
+        <div className='title'>Youtube 2 MP3</div>
     </div>
   )
 }
