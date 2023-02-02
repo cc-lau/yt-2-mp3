0d9c5d129fa7a453cd80e3a5e3cc1c584a202fe0
cc-lau <christianlau03@gmail.com>
2023-02-02 11:54:22 -0700

    Added content container

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div className="content">
       <VideoProvider>
         <Search />
         <DownloadsList />
       </VideoProvider>
       <ExampleDescription />
+      </div>
       <Footer />
     </div>
   );
