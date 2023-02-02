9bcd2be12e885667ba6496a35c23ad673a7b8f85
cc-lau <christianlau03@gmail.com>
2023-02-02 11:57:40 -0700

    Imported stylesheet and rearranged information display

--- src/Components/DownloadsList/VideoCard.js
+++ src/Components/DownloadsList/VideoCard.js
@@ -1,21 +1,23 @@
 import React from "react";
+import "./CardStyles.css"
 
 function VideoCard(props) {
   return (
-    <div>
-      <div>
-        {props.title} {props.duration}{" "}
+    <div className="video-card">
+      <div className="video-title">
+        {props.title}{" "}
+        </div>
         <a
           target="_blank"
           rel="noreferrer"
           href={props.downloadLink}
           download
-          className="download_btn"
+          className="download-link"
         >
           Download MP3
         </a>
+        <div className="video-duration">{props.duration}</div>
       </div>
-    </div>
   );
 }
 
